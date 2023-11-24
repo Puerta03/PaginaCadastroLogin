1d062a40627d7ee7693ab7329e2bbfa1f809961b
Puerta03 <133425034+Puerta03@users.noreply.github.com>
2023-11-24 16:27:24 -0300

    Add files via upload

--- ProjetoSemestral/main.js
+++ ProjetoSemestral/main.js
@@ -9,3 +9,32 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function validateLogin() {
+	const username = document.getElementById('loginUsername').value;
+	const password = document.getElementById('loginPassword').value;
+	// var formulario = document.getElementById('loginForm');
+
+	if (username === '' || password === '') {
+		alert('Por favor, preencha todos os campos.');
+	} else {
+		alert('Login realizado com sucesso!');
+	}
+	// formulario.reset();
+}
+
+function validateSignup() {
+	const username = document.getElementById('signupUsername').value;
+	const email = document.getElementById('signupEmail').value;
+	const password = document.getElementById('signupPassword').value;
+	const confirmPassword = document.getElementById('signupConfirmPassword').value;
+
+	if (username === '' || email === '' || password === '' || confirmPassword === '') {
+		alert('Por favor, preencha todos os campos.');
+	} else if (password !== confirmPassword) {
+		alert('As senhas não coincidem.');
+	} else {
+		alert('Cadastro realizado com sucesso!');
+	}
+
+}
